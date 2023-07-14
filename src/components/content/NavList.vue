ddb7a322a794275e81563775c70fccbed0e5d7fb
Cariyar <465073452@qq.com>
2023-07-14 18:39:36 +0800

    修改从服务器获取导航

--- src/components/content/NavList.vue
+++ src/components/content/NavList.vue
@@ -1,75 +1,83 @@
 <template>
-    <ElMenu class="el-menu-vertical-demo" :default-active="defaultActive">
-        <div v-for="el in menu" :key="el.id">
-            <ElMenuItem v-if="el.menu === undefined" :index="el.index">
-                {{ el.name }}
-            </ElMenuItem>
-            <ElSubMenu v-else :index="el.index">
-                <template #title>
-                    <span>
-                        {{ el.name }}
-                    </span>
-                </template>
-                <ElMenuItemGroup>
-                    <ElMenuItem
-                        v-for="subEl in el.menu"
-                        :key="subEl.id"
-                        :index="subEl.index"
-                    >
-                        {{ subEl.name }}
-                    </ElMenuItem>
-                </ElMenuItemGroup>
-            </ElSubMenu>
-        </div>
-    </ElMenu>
+	<ElMenu class="el-menu-vertical-demo" 
+		:default-active="defaultActive"
+		@open="handleOpen"
+		@close="handleClose"
+	>
+		<div v-for="el in menu" :key="el.id">
+			<ElMenuItem v-if="el.menu === undefined" :index="el.index">
+				{{ el.name }}
+			</ElMenuItem>
+			<ElSubMenu v-else :index="el.index">
+				<template #title>
+					<span>
+						{{ el.name }}
+					</span>
+				</template>
+				<ElMenuItemGroup>
+					<ElMenuItem
+						v-for="subEl in el.menu"
+						:key="subEl.id"
+						:index="subEl.index"
+					>
+						{{ subEl.name }}
+					</ElMenuItem>
+				</ElMenuItemGroup>
+			</ElSubMenu>
+		</div>
+	</ElMenu>
 </template>
 
 <script>
-import { ref,reactive } from "vue";
+import { ref, reactive, onMounted } from "vue";
 import { ElMenu } from "element-plus";
-import { useRouter } from "vue-router";
-// import axios from "axios";
+import axios from "axios";
 export default {
-    name: "NavList",
-    components: {
-        ElMenu,
-    },
-    mounted() {
-        let router = useRouter();
-        console.log(router);
-        // axios({
-        //     method: "post",
-        // });
-    },
-    data() {
-        return {
-            defaultActive: ref("1"),
-            menu: reactive([
-                {
-                    name: "1",
-                    id: 1,
-                    index: "1",
-                },
-                {
-                    name: "2",
-                    id: 2,
-                    index: "2",
-                    menu: [
-                        {
-                            name: "2-1",
-                            id: 21,
-                            index: "2-1",
-                        },
-                        {
-                            name: "2-2",
-                            id: 22,
-                            index: "2-2",
-                        },
-                    ],
-                },
-            ]),
-        };
-    },
+	name: "NavList",
+	components: {
+		ElMenu,
+	},
+	setup() {
+		// data
+		let defaultActive = ref("1");
+		const menu = reactive([]);
+		
+		// methods
+		const handleOpen = (key, keyPath) => {
+			console.log(key, keyPath);
+		};
+		const handleClose = (key, keyPath) => {
+			console.log(key, keyPath);
+		};
+
+		onMounted(() => {
+			let loc = history.state.current;
+			axios({
+				method: "get",
+				url: "/blogserve/nav" + loc,
+			}).then(
+				(res) => {
+					console.log(res.data);
+					let newMenu = reactive(res.data);
+					Object.assign(menu, newMenu);
+				},
+				(err) => {
+					console.log(err);
+				}
+			);
+
+		})
+		
+		return {
+			// data
+			defaultActive,
+			menu,
+
+			// methods
+			handleOpen,
+			handleClose
+		}
+	}
 };
 </script>
 
