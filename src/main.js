745bc4f0530f982d60deb3b07183aa7cce032f09
Cariyar <465073452@qq.com>
2023-11-14 20:47:42 +0800

    添加环境配置

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue';
 import router from "./router/index.js";
 // 引入vuex
 // 小项目用不着，只是试试
-// import store from "./store"
+import store from "./store"
 
 // 引入ElementUI
 import ElementPlus from "element-plus";
@@ -14,7 +14,7 @@ const app = createApp(App);
 // 使用路由
 app.use(router);
 // 使用vuex
-// app.use(store);
+app.use(store);
 // 使用elementPlus
 app.use(ElementPlus);
 // 挂载
